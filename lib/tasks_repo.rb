7a0cfb3ffb87c18d296bfaee537b6efb3df0c031
Rachel Logie <rachel.logie1@gmail.com>
2014-04-03 19:55:38 -0600

    Refactor for better organization and readability, DRY

--- lib/tasks_repo.rb
+++ lib/tasks_repo.rb
@@ -1,24 +1,15 @@
 require 'sequel'
 
 class TasksRepo
+  def initialize(db)
+    @all_tasks = db[:tasks]
+  end
+
   def create(task)
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
-
-    db.create_table! :tasks do
-      primary_key :id
-      String :description
-      Boolean :completed, default: false
-    end
-
-    tasks = db[:tasks]
-
-    tasks.insert(task)
-
+    @all_tasks.insert(task)
   end
 
   def all
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
-    tasks = db[:tasks]
-    tasks.to_a
+    @all_tasks.to_a
   end
 end
--- spec/tasks_repo_spec.rb
+++ spec/tasks_repo_spec.rb
@@ -2,12 +2,22 @@ require 'tasks_repo'
 
 describe TasksRepo do
 
+  before do
+    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
+
+    db.create_table! :tasks do
+      primary_key :id
+      String :description
+      Boolean :completed, default: false
+    end
+    @tasks_repo = TasksRepo.new(db)
+  end
+
   it "can create tasks" do
 
-    repo = TasksRepo.new
     description = "Do #{rand(2..20)} stuff"
-    repo.create(description: description)
-    tasks = repo.all
+    @tasks_repo.create(description: description)
+    tasks = @tasks_repo.all
     expect(tasks).to eq(
       [
         {id: 1, description: description, completed: false}
